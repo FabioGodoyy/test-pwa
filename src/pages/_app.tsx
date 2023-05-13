e2aafabff3e1e47a774d0187391bc14579ee8e63
FabioGodoyy <fabiogodoy@Fabio>
2023-05-13 09:33:37 -0300

    testaaando

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,11 +6,18 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link rel='shortcut icon' href='/vercel.svg' />
+        {/* <link rel='shortcut icon' href='favicon/vercel.svg' />
         <link rel="apple-touch-icon" sizes="152x152" href="/icons/touch-icon-ipad.png/vercel.svg" />
         <link rel="manifest" href="/manifest.json" />
-        {/* <meta rel="theme-color" content='#06092B' /> */}
-        <meta name="theme-color" content='#06092B' />
+        <meta name="theme-color" content='#06092B' /> */}
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/vercel.svg"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/vercel.svg"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/vercel.svg"/>
+        <link rel="manifest" href="/manifest.json"/>
+        <link rel="mask-icon" href="/favicon/vercel.svg" color="#5bbad5"/>
+        <meta name="msapplication-TileColor" content="#da532c"/>
+        <meta name="theme-color" content="#ffffff"/>
       </Head>    
       <Component {...pageProps} />
     </>
